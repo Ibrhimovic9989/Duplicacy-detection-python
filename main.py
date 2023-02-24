e8a23c735fd35275350740aa85f6e0be955e12cf
Ibrhimovic9989 <101934275+Ibrhimovic9989@users.noreply.github.com>
2023-02-24 18:17:29 +0530

    Update main.py
    added comments

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
+#Importing the required libraries
 import pandas as pd
-from openpyxl import load_workbook
-import xlsxwriter
+
+#Taking input from user for file path, sheet name and aadhaar number
 
 file_path = input("Enter the file path:")
 sheet_name = input("Enter the file name:")
-A= input("Enter the aadhaar number:")
-df=pd.read_excel(file_path,engine='openpyxl')
-df1=df
-print(df1)
+A = input("Enter the aadhaar number:")
 
+#Reading the excel file into pandas dataframe
+
+df = pd.read_excel(file_path, engine='openpyxl')
+df1 = df
+
+#Checking if the aadhaar number is already present in the dataframe
 
 if A in df1.values:
-   print("ALready Registered")
-
+  print("Already registered")
 else:
-    a = input("Enter your name:")
-    b = input("Enter your Gender:")
-    c = input("Enter your Phone Number:")
-    data = {'Aadhaar Number': [A],
-            'Name': [a],
-            'Gender': [b],
-            'PHONE NUMBER': [c]}
-    df2 = pd.DataFrame(data)
-    df3 = df1.append(df2)
-    writer = pd.ExcelWriter(file_path, engine='openpyxl')
-    df3.to_excel(writer, sheet_name, index=False)
-    writer.save()
+# If aadhaar number is not present, taking input for name, gender and phone number
+  a = input("Enter your name:")
+  b = input("Enter your Gender:")
+  c = input("Enter your Phone Number:")
 
 
+# Creating a dictionary of the new data to be added and converting it to pandas dataframe
+data = {'Aadhaar Number': [A],
+        'Name': [a],
+        'Gender': [b],
+        'PHONE NUMBER': [c]}
+df2 = pd.DataFrame(data)
 
-
+# Appending the new data to the original dataframe and writing it to the same excel file
+df3 = df1.append(df2)
+writer = pd.ExcelWriter(file_path, engine='openpyxl')
+df3.to_excel(writer, sheet_name=sheet_name, index=False)
+writer.save() 
 
 
